d89624fe713b145dcb02b59b2107747688544c5a
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-25 17:59:35 +0900

    build.sh を更新

--- build.sh
+++ build.sh
@@ -42,8 +42,8 @@ then
     git clone https://github.com/emscripten-core/emsdk.git
     cd emsdk
     git pull
-    ./emsdk install 3.1.20
-    ./emsdk activate 3.1.20
+    ./emsdk install 3.1.10
+    ./emsdk activate 3.1.10
     cd ..
 fi
 
