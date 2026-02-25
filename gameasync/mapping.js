8035113f3d7bcd296d96ec00ee438ae2f00042f4
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-25 00:21:32 +0000

    Deploying to built from @ masada-tea/mkxp-web@ed552bfc093568b2819e0e5df2fd63f844527463 🚀

--- gameasync/mapping.js
+++ gameasync/mapping.js
@@ -1101,8 +1101,8 @@ var mappingArray = [
 ["graphics/transitions/020-flat01", "Graphics/Transitions/020-Flat01.png?h=94dd9f75005cdc7236a0b400b71920fd"],
 ["graphics/windowskins", "Graphics/Windowskins?h="],
 ["graphics/windowskins/windowskin_kn", "Graphics/Windowskins/Windowskin_kn.png?h=6ad592d1b718424e634dda8f7b96aac2"],
-["bitmap-map", "bitmap-map.js?h=fae0a0752442db1ff4848e60b6db207a"],
-["mapping", "mapping.js?h=5ff79b4315b083d80502dacf0af84bac"],
+["bitmap-map", "bitmap-map.js?h=81559f4854a0d619dc8fe97d49f7bfa0"],
+["mapping", "mapping.js?h=7f80dd9bd8f692847923ec19f007996b"],
 ["rgss", "rgss.rb?h=e4f6ce1cd6617eedd6e4704cfd0d263b"],
 ];
 
